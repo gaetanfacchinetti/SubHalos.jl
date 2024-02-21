fabae959990142cafa7f986220ed601226234946
Gaétan Facchinetti <gaetanfacc@gmail.com>
2024-02-21 16:25:44 +0100

    starting code

--- src/SubHalos.jl
+++ src/SubHalos.jl
@@ -1,5 +1,77 @@
 module SubHalos
 
-# Write your package code here.
+import QuadGK, Roots
+
+import CosmoTools : median_concentration, SCP12, MassConcentrationModel, gravitational_potential
+import CosmoTools : ρ_halo, mΔ, planck18, MPC_TO_KM, orbital_frequency, G_NEWTON, KM_TO_MPC
+import HostHalos : HostModel, number_circular_orbits
+
+include("./TidalStripping.jl")
+
+
+export subhalo_mass_function_template
+export mass_function_merger_tree
+
+
+struct FSLParams
+    Mmin::T
+    αm::T
+    ϵt::T
+end
+
+#############################################################
+# Defnitions of basic functions
+
+@doc raw""" 
+    subhalo_mass_function_template(x, γ1, α1, γ2, α2, β, ζ)
+
+Template function for the subhalo mass function:
+
+``m_Δ^{\rm host} \frac{\partial N(m_Δ^{\rm sub}, z=0)}{\partial m_Δ^{\rm sub}} = \left(\gamma_1 x^{-\alpha_1} + \gamma_2 x^{-\alpha_2}\right)  e^{-\beta x^\zeta}``
+
+The first argument, `x::Real`, is the ratio of the subhalo over the host mass ``m_Δ^{\rm sub} / m_Δ^{\rm host}.``
+"""
+function subhalo_mass_function_template(x::Real, γ1::Real,  α1::Real, γ2::Real, α2::Real, β::Real, ζ::Real)
+    return (γ1*x^(-α1) + γ2*x^(-α2)) * exp(-β * x^ζ )
+end
+
+
+@doc raw"""
+    pdf_virial_mass(mΔ_sub, mΔ_host)
+
+Example of subhalo mass function fitted on merger tree results
+(Facchinetti et al., in prep.)
+
+# Arguments
+- `mΔ_sub::Real` : subhalo virial mass (in Msun)
+- `mΔ_host::Real`: host virial mass (in Msun)
+
+# Returns
+- ``\frac{\partial N(m_Δ^{\rm sub}, z=0)}{\partial m_Δ^{\rm sub}}``
+"""
+function density_virial_mass(mΔ_sub::Real, mΔ_host::Real, z_acc::Real = 0) 
+    return subhalo_mass_function_template(mΔ_sub / mΔ_host, 0.019, 1.94, 0.464, 1.58, 24.0, 3.4)/mΔ_host
+end
+
+std_mass_concentration(m::Real, ::Type{SCP12}) = 0.14 * log(10.0)
+std_mass_concentration(m::Real, ::Type{T}) where {T<:MassConcentrationModel} = std_mass_concentration(m, T)
+
+function pdf_concentration(c200::Real, m200::Real, z::Real = 0, cosmo::Cosmology = planck18, ::Type{T} = SCP12) where {T<:MassConcentrationModel}
+   
+    σ_c = std_mass_concentration(m200, T)
+    median_c = median_concentration(m200, z, cosmo, T)
+
+    Kc = 0.5 * erfc(-log(median_c) / (sqrt(2.0) * σ_c))
+
+    return 1.0 / Kc / c200 / sqrt(2.0 * π) / σ_c * exp(-(log(c200) - log(median_c)) / sqrt(2.0) / σ_c^2)
+end
+
+
+#m200 in Msol
+pdf_position(r::Real, host::HostModel, cosmo::Cosmology = planck18) = 4 * π  * r^2 * ρ_halo(r, host.halo) / mΔ(host.halo, 200, cosmo.bkg.ρ_c0)
+density_virial_mass(m200::Real, host::HostModel, z = 0, cosmo::Cosmology = planck18) = pdf_virial_mass(m200, mΔ(host.halo, 200, cosmo.bkg.ρ_c0), z) 
+
+number_subhalos(host::HostModel, z=0, cosmo::Cosmology = planck18) =  QuadGK.quadgk(lnm -> exp(lnm))
+
 
 end
---
+++ src/TidalStripping.jl
@@ -0,0 +1,92 @@
+export jacobi_scale, disk_shocking_tidal_radius
+
+################################################
+## Jacobi Radius
+
+@doc raw"""
+    jacobi_scale(r, ρs, hp, ρ_host, m_host)
+
+Jacobi scale radius for a subhalo of scale density `ρs` (in Msol/Mpc^3) with a HaloProfile `hp`
+at a distance r from the host centre  such that the sphericised mass density of the host at r is `ρ_host` 
+and the sphericised enclosed mass inside the sphere of radius r is `m_host`.
+
+More precisely, returns `xt` solution to
+``\frac{xt^3}{\mu(xt)} - \frac{\rho_{\rm s}}{\rho_{\rm host}(r)} \frac{\hat \rho}{1-\hat \rho}``
+with `` reduced sperical host density being
+`` = \frac{4\pi}{3} r^3 \frac{\rho_{\rm host}(r)}{m_{\rm host}(r)}``
+"""
+function jacobi_scale(r::Real, ρs::Real, hp::HaloProfile, ρ_host::Real, m_host::Real)
+    reduced_ρ =  4 * π * r^3 *  ρ_host / 3.0 / m_host
+    to_zero(xt::Real) = xt^3/μ_halo(xt, hp) - ρs/ρ_host * reduced_ρ / (1.0 - reduced_ρ)
+    return exp(Roots.find_zero(lnxt -> to_zero(exp(lnxt)), (-5, +5), Roots.Bisection())) 
+end
+
+
+jacobi_scale(r::Real, ρs::Real, hp::HaloProfile, host::HostModel{<:Real}) = jacobi_scale(r, ρs, hp, host.ρ_host_spherical(r), host.m_host_spherical(r))
+jacobi_scale(r::Real, subhalo::Halo{<:Real}, host::HostModel{<:Real}) = jacobi_scale(r, subhalo.ρs, subhalo.hp, host)
+jacobi_scale(r::Real, subhalo::Halo{<:Real}, ρ_host::Real, m_host::Real) = jacobi_scale(r, subhalo.ρs, subhalo.hp,  ρ_host, m_host)
+
+jacobi_radius(r::Real, subhalo::Halo{<:Real}, host::HostModel{<:Real}) = subhalo.rs * jacobi_scale(r, subhalo.ρs, subhalo.hp, host)
+
+
+################################################
+## Disk shocking
+
+adiabatic_correction(η::Real) = (1+η^2)^(-3/2)
+
+function adiabatic_correction(r_sub::Real, r_host::Real, subhalo::Halo{<:Real}, host::HostModel{<:Real})
+    hd = host.stars.thick_zd
+    σz = host.circular_velocity_kms(r_host) / sqrt(2)
+    td = hd * MPC_TO_KM / σz # in s
+    ωd = orbital_frequency(r_sub, subhalo) # in s^{-1}
+    η = td * ωd
+    return adiabatic_correction(η)
+end
+
+## NEED TO IMPLEMENT σ_baryons
+""" Energy gained by particle mass in a single crossing of th disk (in Mpc / s)^2 """
+function angle_average_energy_shock(r_sub::Real, r_host::Real, subhalo::Halo{<:Real}, host::HostModel{<:Real})
+    disc_acceleration = 2.0 * π * G_NEWTON * host.σ_baryons(r_host) # in Mpc / s^2
+    σz = host.circular_velocity_kms(r_host) * KM_TO_MPC / sqrt(2) # in Mpc / s
+    return 2 * (disc_acceleration / σz * r_sub)^2 * adiabatic_correction(r_sub, r_host, subhalo, host) / 3.0;
+end
+
+""" Tidal radius after one crossing of the disk in units of the scale radius """
+function disk_shocking_tidal_radius(x_init::Real, r_host::Real, subhalo::Halo{<:Real}, host::HostModel{<:Real})
+    _to_bissect(x::Real) = angle_average_energy_shock(x * subhalo.rs, r_host, subhalo, host) / abs(gravitational_potential(x, x_init, subhalo.hp)) -1.0
+    return exp(Roots.find_zero(lnx -> _to_bissect(exp(lnx)), (1e-6, x_init), Roots.Bisection()))
+end
+
+""" Tidal radius after n_cross crossing of the disk in units of the scale radius"""
+function disk_shocking_tidal_radius(x_init::Real, r_host::Real, subhalo::Halo{<:Real}, host::HostModel{<:Real}, n_cross::Int)
+
+    xt = x_init
+
+    for i in 1:n_cross
+        (xt < 1e-6) && return 0.0
+        xt = disk_shocking_tidal_radius(xt, r_host, subhalo, host)
+    end
+
+    return xt
+end
+
+#########################################
+# Total baryonic tides
+
+# Need to add stellar encounters here afterwards
+function baryonic_tides(x_init::Real, r_host::Real, subhalo::Halo{<:Real}, host::HostModel{<:Real}, n_cross::Int)
+    return disk_shocking_tidal_radius(x_init, r_host, subhalo, host, n_cross)
+end
+
+function tidal_scale(r_host::Real, subhalo::Halo{<:Real}, z::Real = 0, cosmo::Cosmology = planck18)
+
+    xt = jacobi_scale(r_host, subhalo, host)
+    n_cross = number_circular_orbits(r_host, host, z, cosmo)
+    return baryonic_tides(xt, r_host, subhalo, host, n_cross)
+
+end
+
+tidal_radius(r_host::Real, subhalo::Halo{<:Real}, z::Real = 0, cosmo::Cosmology = planck18) = tidal_scale(r_host, subhalo, z, cosmo) * subhalo.rs
+
+
+## IMPLEMENT σ_baryons
